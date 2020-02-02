e369352b97c923b9a76c0a4edf1115204d279a0c
Lior Rabinovich <lior.rabinovich@nex.com>
2020-02-02 11:30:37 +0200

    Added comments

--- deepFindKey.js
+++ deepFindKey.js
@@ -2,6 +2,13 @@ export function isObject(object) {
     return typeof object === 'object' && object !== null;
 }
 
+/**
+ * Deep Find Key
+ * @description Get the object I found by key inside an object with children objects
+ * @param {Object} object
+ * @param {String} key
+ * @returns {Object}
+ */
 export function deepFindKey(object, key) {
     if (!isObject(object)) return object;
     if (object[key]) return { [object[key]]: object };
